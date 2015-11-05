d7e2cb0b523d2f61b901eea178cb1f0cab619d9e
aselims <selim.2k@gmail.com>
2015-11-05 21:44:35 +0100

    add recyclerView

--- app/src/main/java/co/rahala/traktino/top10/TopTenFragment.java
+++ app/src/main/java/co/rahala/traktino/top10/TopTenFragment.java
@@ -2,6 +2,7 @@ package co.rahala.traktino.top10;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,7 +23,9 @@ public class TopTenFragment extends Fragment implements TopTenContract.View {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View root = inflater.inflate(R.layout.fragment_top_ten, container, false);
+        RecyclerView recyclerView = (RecyclerView) root.findViewById(R.id.movies_list);
+        return root;
     }
 
     @Override
