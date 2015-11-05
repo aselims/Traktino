2c36066629a0caf7e8629c611a172e0d501287db
aselims <selim.2k@gmail.com>
2015-11-05 23:28:42 +0100

    Model

---
+++ app/src/main/java/co/rahala/traktino/data/Images.java
@@ -0,0 +1,57 @@
+package co.rahala.traktino.data;
+
+/**
+ * Created by aselims on 05/11/15.
+ */
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+public class Images {
+
+    @SerializedName("poster")
+    @Expose
+    private Poster poster;
+
+    /*@SerializedName("fanart")
+        @Expose
+        private Fanart fanart;
+
+        @SerializedName("logo")
+        @Expose
+        private Logo logo;
+        @SerializedName("clearart")
+        @Expose
+        private Clearart clearart;
+        @SerializedName("banner")
+        @Expose
+        private Banner banner;
+        @SerializedName("thumb")
+        @Expose
+        private Thumb thumb;
+*/
+
+    public Poster getPoster() {
+        return poster;
+    }
+
+    class Poster {
+        /* @SerializedName("full")
+         @Expose
+         private String full;
+         @SerializedName("medium")
+         @Expose
+         private String medium;
+        */
+
+        @SerializedName("thumb")
+        @Expose
+        private String thumb;
+
+        public String getThumb() {
+            return thumb;
+        }
+    }
+
+}
--- app/src/main/java/co/rahala/traktino/data/Movie.java
+++ app/src/main/java/co/rahala/traktino/data/Movie.java
@@ -1,7 +1,81 @@
 package co.rahala.traktino.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by aselims on 05/11/15.
  */
 public class Movie {
+
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("year")
+    @Expose
+    private Integer year;
+    /*@SerializedName("ids")
+    @Expose
+    private Ids ids;*/
+    @SerializedName("tagline")
+    @Expose
+    private String tagline;
+    @SerializedName("overview")
+    @Expose
+    private String overview;
+    @SerializedName("released")
+    @Expose
+    private String released;
+    @SerializedName("runtime")
+    @Expose
+    private Integer runtime;
+    @SerializedName("trailer")
+    @Expose
+    private String trailer;
+    @SerializedName("homepage")
+    @Expose
+    private Object homepage;
+    @SerializedName("rating")
+    @Expose
+    private Double rating;
+    @SerializedName("votes")
+    @Expose
+    private Integer votes;
+    @SerializedName("updated_at")
+    @Expose
+    private String updatedAt;
+    @SerializedName("language")
+    @Expose
+    private String language;
+    @SerializedName("available_translations")
+    @Expose
+    private List<String> availableTranslations = new ArrayList<String>();
+    @SerializedName("genres")
+    @Expose
+    private List<String> genres = new ArrayList<String>();
+    @SerializedName("certification")
+    @Expose
+    private String certification;
+    @SerializedName("images")
+    @Expose
+    private Images images;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public Images getImages() {
+        return images;
+    }
 }
