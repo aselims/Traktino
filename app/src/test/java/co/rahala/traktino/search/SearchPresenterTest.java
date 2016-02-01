9968b5c8394ebbced8257a6353bf5242b3d47398
aselims <selim.2k@gmail.com>
2016-02-01 20:43:52 +0100

    travis

---
+++ app/src/test/java/co/rahala/traktino/search/SearchPresenterTest.java
@@ -0,0 +1,33 @@
+package co.rahala.traktino.search;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by aselims on 18/12/15.
+ */
+public class SearchPresenterTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testLoadSearch() throws Exception {
+
+    }
+
+    @Test
+    public void testCancel() throws Exception {
+
+    }
+}
